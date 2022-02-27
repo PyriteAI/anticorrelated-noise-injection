caf3e049180e20517426e17335d2f1a575bddbab
Miller Wilt <miller@pyriteai.com>
2022-02-27 16:58:33 -0500

    feat: add initial implementation of ACNI

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url="https://github.com/PyriteAI/anticorrelated-noise-injection",
     keywords=["artificial intelligence", "machine learning", "jax", "optax", "generalization", "noise injection"],
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.8",
